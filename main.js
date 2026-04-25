bcb0d7411764d57098cd71ffa3ccde9ca1b214e1
NaughtySora <naughtysora@proton.me>
2026-04-25 16:25:57 +0300

    refactor core, docker, readme, tests

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const { abstract } = require("naughty-util");
 const { resolve } = require("node:path");
 
 const approach = abstract.factorify({
-  process: resolve(__dirname, "./process.js"),
-  thread: resolve(__dirname, "./thread.js"),
-}, resolve(__dirname, "./monitor.js"));
+  process: resolve(__dirname, "./lib/process.js"),
+  thread: resolve(__dirname, "./lib/thread.js"),
+}, resolve(__dirname, "./lib/scheduler.js"));
 
 module.exports = (parameters = {}) =>
   require(approach(parameters.isolate))(parameters);
--- test/index.js
+++ test/index.js
@@ -1,54 +1,52 @@
 "use strict";
-
-const scheduler = require("../lib/index.js");
-const redis = require("redis");
-const path = require("node:path");
+process.loadEnvFile(".env");
+const scheduler = require("../main.js");
+const { createClient } = require("redis");
+const { resolve } = require("node:path");
+const { describe, it } = require("node:test");
+const api = require('./api.js');
+const { async } = require("naughty-util");
 
 const KEY = "jobs";
 
-const main = async () => {
-  const client = redis.createClient();
-  await client.connect();
-
-  const { start, stop } = scheduler({
-    path: path.resolve(__dirname, "./api.js"),
-    options: { key: KEY, interval: 1000 },
-    isolate: "process",
-  });
-
-  await start();
-
-  const timer = setTimeout(async () => {
-    const score = Date.now();
-    await client.zAdd(KEY, {
-      score,
-      value: JSON.stringify({ name: "log", params: [`log-${score}`] }),
-    });
-    timer.refresh();
-  }, 1000);
-
-  const exit = async () => {
-    clearTimeout(timer);
-    if (client.isOpen) await client.close();
-    await stop();
-  };
-
-  process.on('SIGINT', async () => {
-    try {
-      await exit();
-      console.log("Grateful exit");
-      process.exit(0);
-    } catch {
-      console.error("Error while exiting");
-      process.exit(1);
-    }
-  });
-
-  process.on("uncaughtException", async (error) => {
-    await exit();
-    console.error("Application closed with", error);
-    process.exit(1);
-  });
+const config = {
+  password: process.env.REDIS_PASSWORD,
+  port: parseInt(process.env.REDIS_PORT, 10),
 };
 
-main();
+describe("Scheduler - Main Thread", async () => {
+  await it("simple", async () => {
+    const redis = createClient(config);
+    const client = scheduler({
+      key: KEY,
+      interval: 1000,
+      modules: { api },
+    });
+    client.on("error", console.error);
+    await client.start(redis);
+    client.add(JSON.stringify({
+      name: "api",
+      key: "log",
+      args: [],
+    }));
+    await async.pause(2000);
+    await client.stop();
+  });
+});
+
+
+// process.on('SIGINT', async () => {
+//   try {
+//     await client.stop();
+//     console.log("Grateful exit");
+//     process.exit(0);
+//   } catch (e) {
+//     console.error("Error while exiting", e);
+//     process.exit(1);
+//   }
+// });
+// process.on("uncaughtException", async (error) => {
+//   await exit();
+//   console.error("Application closed with", error);
+//   process.exit(1);
+// });
